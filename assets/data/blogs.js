f6f865e630e43b402c3c15200a329f51cc705e9a
Mehedi Hasan Likhon <mehedi.hasansjs@gmail.com>
2020-07-31 17:28:57 +0600

    Add files via upload

---
+++ assets/data/blogs.js
@@ -0,0 +1,29 @@
+export const blogs = [
+  {
+    img: "https://cdn.vox-cdn.com/thumbor/6TohF9AFbaiUAOXzlOg6UxCUQqg=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/16213725/git.jpg",
+    title: "GIT Crash Course",
+    desc: "Git is a free, open-source distributed version control software that lets you manage and keep track of your...",
+    time: `${new Date().getUTCFullYear() - 2020} years and ${new Date().getMonth() - 6} months ago`,
+    link: ""
+  }, {
+    img: "https://res.cloudinary.com/practicaldev/image/fetch/s--4bkS0tHe--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/ij442nmts27tc6xcfoq9.png",
+    title: "Angular Architecture",
+    desc: "Building scalable software is a challenging task. When we think about scalability in front-end applications...",
+    time: `${new Date().getUTCFullYear() - 2020} years and ${new Date().getMonth() - 6} months ago`,
+    link: ""
+  },
+  {
+    img: "https://res.cloudinary.com/practicaldev/image/fetch/s--TQJZ7jgo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--8uVvrcE3--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://thepracticaldev.s3.amazonaws.com/i/teqsgstj33n4zgcb0688.png",
+    title: "Observables in NG",
+    desc: "Angular is an object-oriented framework. lot of things imperatively  and therefore also some, are reactive...",
+    time: `${new Date().getUTCFullYear() - 2020} years and ${new Date().getMonth() - 6} months ago`,
+    link: ""
+  },
+  {
+    img: "https://res.cloudinary.com/practicaldev/image/fetch/s--aXvgn4sd--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--zav9iWM0--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://dev-to-uploads.s3.amazonaws.com/i/8b14k3gdzzdvm6ubheo2.jpg",
+    title: "Awesome Web Effect",
+    desc: "It's like decorating a beautiful Christmas tree in a room. Sometimes a small web page with cool effects will ...",
+    time: `${new Date().getUTCFullYear() - 2020} years and ${new Date().getMonth() - 6} months ago`,
+    link: ""
+  }
+];
